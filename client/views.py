234f000c0e9ca37232ada89e822c965b5e727bce
Kuma Stéphane <kumastephane@gmail.com>
2022-04-22 16:40:29 +0000

    Client viewset edited

--- client/views.py
+++ client/views.py
@@ -13,3 +13,14 @@ class ClientViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return self.queryset.filter(created_by=self.request.user)
     
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def perform_update(self, serializer):
+        obj = self.get_object()
+
+        if self.request.user != obj.created_by:
+            raise PermissionError('Wrong object owner')
+        
+        serializer.save()
+    
