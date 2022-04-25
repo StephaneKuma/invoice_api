30884334abb04527d41772613d8ecb8954c59392
Kuma Stéphane <kumastephane@gmail.com>
2022-04-25 13:22:19 +0000

    Client and Invoice relationship created

--- client/serializers.py
+++ client/serializers.py
@@ -2,8 +2,29 @@ from rest_framework import serializers
 
 from .models import Client
 
+from invoice.models import Invoice
+
+
+class ClientInvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = (
+            "id",
+            "invoice_number",
+            "is_sent",
+            "is_paid",
+            "gross_amount",
+            "vat_amount",
+            "net_amount",
+            "get_due_date_formatted",
+            "invoice_type",
+            "is_credited",
+        )
+
 
 class ClientSerializer(serializers.ModelSerializer):
+    invoices = ClientInvoiceSerializer(many=True)
+
     class Meta:
         model = Client
         managed = True
