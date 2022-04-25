ebc102faf52533d8a5495db9f6f8ea911ee02188
Kuma Stéphane <kumastephane@gmail.com>
2022-04-25 12:23:29 +0000

    Team app urls registered in the project urls

--- settings/urls.py
+++ settings/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('client.urls')),
+    path('api/v1/', include('team.urls')),
 ]
